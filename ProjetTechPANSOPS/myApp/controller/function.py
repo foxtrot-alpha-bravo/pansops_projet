7d18cc898ad730accc89247fc8b1bf7d19c1f158
Fabien <fabien.mavinga@icloud.com>
2025-02-25 22:46:00 +0100

    modified:   ProjetTechPANSOPS/myApp/controller/function.py 	modified:   ProjetTechPANSOPS/myApp/views.py

--- ProjetTechPANSOPS/myApp/controller/function.py
+++ ProjetTechPANSOPS/myApp/controller/function.py
@@ -10,6 +10,48 @@ def format_date(date_str):
     except Exception:
         return " "
     
+def calcul_FC(listeAgents):
+    for k in range(len(listeAgents)): #La fonction sera exécutée pour chaque agent 
+        if listeAgents[k]["derniere_formation"]=='None' : #Si l'agent n'a aucune formation enregistrée
+            listeAgents[k]['couleur'] = 'red' #La couleur revenant dans le dictionnaire est rouge, et sera affichée en conséquence
+        else :
+            date_ajour = datetime.today().date() #La variable est initialisée avec la date du jour
+            date_form = datetime.strptime(listeAgents[k]["derniere_formation"], "%Y-%m-%d").date() #La date présente dans le dictionnaire est appellée par cette variable
+            jours_ecart = (date_form - date_ajour).days #On recherche le nombre de jours entre la date de la dernière formation et la date du jour en faisant une soustraction entre les deux dates
+            listeAgents[k]['jours_ecart'] = jours_ecart #La variable jours_ecart correspondant aux nombres de jours est incluse au dictionnaire
+            
+            #Une couleur est renvoyée en fonction du nombre de jours, qui reflétera l'urgence de la situation de l'agent
+            if jours_ecart >= 365 :
+                listeAgents[k]['couleur'] = 'vert'
+            elif jours_ecart >= 180 :
+                listeAgents[k]['couleur'] = 'jaune'
+            elif jours_ecart >=0 :
+                listeAgents[k]['couleur'] = 'orange'
+            else :
+                listeAgents[k]['couleur'] = 'rouge'
+    return listeAgents
+
+def calcul_MAC(listeAgents,listeMaint):
+    date_ajour = datetime.today().date() #La variable est initialisée avec la date du jour
+    for i in range(len(listeMaint)):  #on redefinie une nouvelle couleur, cette fois-ci pour le maintien de compétences
+        listeMaint[i]['couleurM'] = 'None' #on set la nouvelle clé 'couleurM' pour Maintien de compétences en 'None' par défaut
+        if listeMaint[i]["derniere_formation_3"]=='None' : #on ne check que la 3e dernière formation 
+            listeMaint[i]['couleurM'] = 'red' #si il n'y a pas de 3e dernière formation, la couleur sera rouge
+        else : 
+            date_maint = datetime.strptime(listeMaint[i]["derniere_formation_3"], "%Y-%m-%d").date() 
+            ecart_maint = (date_maint - date_ajour).days
+            listeAgents[i]["jours_ecartM"]=ecart_maint
+            if ecart_maint >= 365 :
+                listeMaint[i]['couleurM'] = 'vert'
+            elif ecart_maint >= 180 :
+                listeMaint[i]['couleurM'] = 'jaune'
+            elif ecart_maint >=0 :
+                listeMaint[i]['couleurM'] = 'orange'
+            else :
+                listeMaint[i]['couleurM'] = 'rouge'
+        listeAgents[i]['couleurM'] = listeMaint[i]['couleurM']
+    return listeAgents
+    
     
 # passe les messages d'info en paramètres
 def messageInfo(params = None):
